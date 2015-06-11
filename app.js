85d53eec627f61eaf848a07fb316d18132c13b77
elenapenso <elenapenso@yahoo.com>
2015-06-11 13:26:19 -0400

    new directive with controller

--- app.js
+++ app.js
@@ -75,7 +75,7 @@
 			reviews: [],
 		}
 		];
-
+/*
 	app.controller('PanelController', function(){
 		this.tab = 1;
 		this.selectTab = function(setTab) {               
@@ -85,7 +85,7 @@
 			return this.tab === checkTab;
 		};
 	});
-	
+*/	
 	app.controller('ReviewController', function(){
 		this.review = {};	
 		this.addReview = function(product) {			
@@ -99,6 +99,23 @@
 			restrict: 'E',
 			templateUrl: 'product-title.html'
 		};	
+	});
+
+	app.directive('productPanels', function(){
+		return{
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+				this.selectTab = function(setTab) {               
+					this.tab = setTab;
+				};
+				this.isSelected = function(checkTab) {
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panels',
+		};
 	});
 
 })();
