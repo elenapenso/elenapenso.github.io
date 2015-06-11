081898e43188ea39858d0c2762575ae46627068f
elenapenso <elenapenso@yahoo.com>
2015-06-11 15:58:07 -0400

    new directive with controller

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 (function() {
 	var app = angular.module('store',['store-products']);
 
-	app.controller('StoreController', ['$http', function($scope, $http){
+	app.controller('StoreController', function($scope, $http){
 	//	var store = this;   /* to recover data from the http request */
 		$scope.products = [];
 
-		$http.get('/products.json').success(function(data){
+		$http.get('products.json').success(function(data){
 			$scope.products = data;
 		});
-	}]);
+	});
 
 	app.controller('ReviewController', function(){
 		this.review = {};	
