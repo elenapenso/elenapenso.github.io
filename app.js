801ba16b61e10befb7d4c6907c357bbe584477c4
elenapenso <elenapenso@yahoo.com>
2015-06-11 14:22:26 -0400

    new directive with controller

--- app.js
+++ app.js
@@ -1,80 +1,15 @@
 (function() {
 	var app = angular.module('store',['store-products']);
 
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
+	app.controller('StoreController', ['http', function($http){
+		var store = this;   /* to recover data from the http request */
+		store.products =[];
 
-	var gems = [
-		{
-			name: 'Hectagonal Rubi',
-			price: 2.95,
-			description: 'This is a very precious gem',
-			canPurchase: false,
-			image: '1.png',
-			reviews: [
-				{
-					stars: 5,
-					body: "I love this product!",
-					author: "joe@doe.com",		
-				},
-				{
-					stars: 1,
-					body: "This product sucks!",
-					author: "tim@hater.com",	
-				}	
+		$http.get('products.json').success(function(data){
+			store.products = data;
 
-			]
-		},
-		{
-			name: 'Emerald Drop',
-			price: 3,
-			description: 'This is a rare to find gem',
-			canPurchase: true,
-			image: '2.png',
-			reviews: [],
-		},
-		{
-			name: 'Jade',
-			price: 1.5,
-			description: 'This is a special power gem',
-			canPurchase: true,
-			image: '3.png',
-			reviews: [],
-		},
-		{
-			name: 'Green Quartz',
-			price: 5.95,
-			description: 'This is a special power gem',
-			canPurchase: true,
-			image: '4.png',
-			reviews: [],
-		},
-		{
-			name: 'Zephyr',
-			price: 8,
-			description: 'This is a special power gem',
-			canPurchase: true,
-			image: '5.png',
-			reviews: [],
-		},
-		{
-			name: 'Triangular Amethyste',
-			price: 12.05,
-			description: 'This is a nobility gem',
-			canPurchase: true,
-			image: '6.png',
-			reviews: [],
-		},
-		{
-			name: 'Royal Pink Diamond',
-			price: 345.20,
-			description: 'Crafted in the mines of Salheim',
-			canPurchase: true,
-			image: '7.png',
-			reviews: [],
-		}
-		];
+		});
+	}]);
 
 	app.controller('ReviewController', function(){
 		this.review = {};	
